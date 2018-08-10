4efb447bd6d7e9822ea18431824deb74d18810ba
Patrick Lehmann <patlehmann1@gmail.com>
2018-08-10 12:30:26 -0400

    setting up the connection

--- config/connection.js
+++ config/connection.js
@@ -7,11 +7,11 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        port: 3306,
-        host: 'localhost',
-        user: 'root',
-        password: 'coast',
-        database: 'burgers_db'
+      host: process.env.DB_HOST,
+      port: 3306,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME
     })
 };
 
--- models/burger.js
+++ models/burger.js
@@ -22,3 +22,9 @@ var burger = {
 };
 
 module.exports = burger;
+
+host: process.env.DB_HOST,
+port: 3306,
+user: process.env.DB_USER,
+password: process.env.DB_PASS,
+database: process.env.DB_NAME
